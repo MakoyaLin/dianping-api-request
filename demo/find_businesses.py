8aa5cabd793c0a21ae409603139e3efd30f3bac2
shengan <qjwgg.guguai@gmail.com>
2015-05-19 15:27:22 +0800

    fix. use the same mongodb collection.

--- demo/find_businesses.py
+++ demo/find_businesses.py
@@ -27,28 +27,7 @@ if __name__ == '__main__':
         for category in categories:
             param['category'] = category
             businesses += api.find_businesses(**param)
-        #break
+        break
     #db.business.insert(businesses)
-
-    business_id_list = []
     for business in businesses:
-        business_id_list.append(str(business['business_id']))
-    business_id_list = set(business_id_list)
-    business_id_list = list(business_id_list)
-    loop_count = len(business_id_list) / 40
-    i = 0
-    batch_businesses = []
-    while i <= loop_count:
-        if i == loop_count:
-            business_ids = business_id_list[i:]
-        else:
-            business_ids = business_id_list[i*40:(i+1)*40]
-        i += 1
-        print business_ids
-        batch_businesses += api.get_batch_businesses_by_id(business_ids)
-    db.batch_business.insert(batch_businesses)
-
-    #for business in businesses:
-        #db.business.update({'business_id': business['business_id']}, {'$set': business}, upsert=True)
-    #    db.business.save({'_id': business['business_id'], 'data': business})
-
+        db.business.update({'business_id': business['business_id']}, {'$set': business}, upsert=True)
--- demo/get_batch_businesses.py
+++ demo/get_batch_businesses.py
@@ -1,26 +1,33 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-import json
+import sys
+sys.path.append("..")
 
 from db import _db as db
-from api_request import api_request
+from dianping import DianpingApi
 
 
 if __name__ == '__main__':
-    # http://developer.dianping.com/app/api/v1/business/get_batch_businesses_by_id
-    apiUrl = "http://api.dianping.com/v1/business/get_batch_businesses_by_id"
-    paramSet = []
-    paramSet.append(("format", "json"))
-    paramSet.append(("business_ids", "6110204,6110205"))
 
-    data = api_request(apiUrl, paramSet)
-    data = json.loads(data, encoding='utf-8')
-    status = data['status']
-    businesses = data['businesses']
-
-    if status == 'OK':
-        #db.business.insert(businesses)
-        for business in businesses:
-            #db.business.update({'business_id': business['business_id']}, {'$set': business}, upsert=True)
-            db.business.save({'_id': business['business_id'], 'data': business})
+    api = DianpingApi('85719554', 'b9aafd8a60e1435faf7ba3389cdb4e9a')
+    business_id_list = []
+    businesses = db.business.find()
+    for business in businesses:
+        business_id_list.append(str(business['business_id']))
+    business_id_list = set(business_id_list)
+    business_id_list = list(business_id_list)
+    loop_count = len(business_id_list) / 40
+    i = 0
+    businesses = []
+    while i <= loop_count:
+        if i == loop_count:
+            business_ids = business_id_list[i:]
+        else:
+            business_ids = business_id_list[i*40:(i+1)*40]
+        i += 1
+        print business_ids
+        businesses += api.get_batch_businesses_by_id(business_ids)
+    #db.batch_business.insert(businesses)
+    for business in businesses:
+        db.business.update({'business_id': business['business_id']}, {'$set': business}, upsert=True)
